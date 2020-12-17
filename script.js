ead58593f4c29c0edb7ea75158b7c4df93335d1f
Derek <nightwing917@hotmail.com>
2020-12-16 19:39:16 -0700

    Grabbing DOM elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuBars = document.getElementById('menu-bars');
+const overLay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+
